145af7ff7831806632fab1087a81c3f35c5b78bb
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2018-08-29 16:29:29 +0100

    [Matlab] Pfad-Initialisierung

--- serroblib_path_init.m
+++ serroblib_path_init.m
@@ -1,2 +1,5 @@
 % Diese Datei ist leer und dient nur zum Auffinden dieses Ordner mit dem
 % Befehl `which('serroblib_path_init.m')`
+
+this_tb_path = fileparts( mfilename('fullpath') );
+addpath(this_tb_path);
