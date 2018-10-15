fffe16f1a01ac73d4bf4e5946ab9948f5dda2780
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2018-10-15 17:52:09 +0100

    Argument um Code-Generierung zu erzwingen

--- serroblib_generate_code.m
+++ serroblib_generate_code.m
@@ -5,6 +5,8 @@
 %   Cell-Array mit Liste der Namen der Roboterstrukturen, für die der Code
 %   erzeugt werden soll. Der Name entspricht dem Schema "SxRRRyyy" mit
 %   x=Anzahl Gelenk-FG und yyy laufende Nummer für "RRR".
+% force [1x1 logical]
+%   Erzwinge die Neu-Generierung des Codes
 % 
 % Vorher: 
 % * Funktion maplerepo_path.m muss vorliegen mit Rückgabe des
@@ -15,7 +17,12 @@
 % Moritz Schappler, moritz.schappler@imes.uni-hannover.de, 2018-08
 % (C) Institut für mechatronische Systeme, Universität Hannover
 
-function serroblib_generate_code(Names)
+function serroblib_generate_code(Names, force)
+
+if nargin < 2
+  force = false;
+end
+
 repopath=fileparts(which('serroblib_path_init.m'));
 
 for i = 1:length(Names)
@@ -28,14 +35,16 @@ for i = 1:length(Names)
   % Maple-Toolbox-Eingabe laden (wurde an anderer Stelle erzeugt)
   % (durch serroblib_generate_mapleinput.m)
   mapleinputfile=fullfile(repopath, sprintf('mdl_%ddof', N), n, 'hd', sprintf('robot_env_%s', n));
-  
+  if ~exist(mapleinputfile, 'file')
+    error('Datei %s existiert nicht. Wurde `serroblib_generate_mapleinput.m` ausgeführt?', fileparts(mapleinputfile) );
+  end
   % Verzeichnisse für die zu erzeugenden Matlab-Funktionen
   outputdir_tb = fullfile(mrp, 'codeexport', n, 'matlabfcn'); % Verzeichnis in der Maple-Toolbox
   outputdir_local = fileparts(mapleinputfile); % Verzeichnis in der Bibliothek
   
   % Prüfe, ob Code schon einmal generiert wurde 
   % (und im Zielverzeichnis vorliegt)
-  if length(dir(fullfile(outputdir_local, '*.m'))) > 20
+  if ~force && length(dir(fullfile(outputdir_local, '*.m'))) > 20
     % das werden wohl schon genug .m-Dateien sein.
     continue
   end
