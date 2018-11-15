fc71a352f1fc3c2392745d5ccaafd0091aec554d
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2018-11-15 16:41:01 +0100

    Prüfung des Variablentyps der Eingabe

--- serroblib_addtopath.m
+++ serroblib_addtopath.m
@@ -1,9 +1,17 @@
 % Füge die Modelle gegebener Roboter zum Pfad hinzu
+% 
+% Eingabe:
+% Names
+%   Cell-Array mit Namen der Robotermodelle, deren Funktionen zum
+%   Matlab-Pfad hinzugefügt werden sollen.
 
 % Moritz Schappler, moritz.schappler@imes.uni-hannover.de, 2018-08
 % (C) Institut für mechatronische Systeme, Universität Hannover
 
 function serroblib_addtopath(Names)
+if ~iscell(Names)
+  error('serroblib_addtopath: Eingegebene Roboternamen müssen ein Cell-Array sein');
+end
 repopath=fileparts(which('serroblib_path_init.m'));
 for i = 1:length(Names)
   n = Names{i};
