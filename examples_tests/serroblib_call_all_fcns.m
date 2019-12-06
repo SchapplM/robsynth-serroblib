588cb866cf884f1e75f49f707131f938218b22e0
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2019-12-06 11:23:29 +0100

    Aufruf neuer Funktionen mit DynPar-Methode 3 aus SerRobLib.

--- examples_tests/serroblib_call_all_fcns.m
+++ examples_tests/serroblib_call_all_fcns.m
@@ -51,33 +51,83 @@ for N = 1:7
       RS.jacobiaD(q0,qD0);
       RS.jtraf(q0);
       RS.jointvar(q0);
-
+      
       % Rufe Dynamik-Funktionen mit Inertialparametern als Eingang auf
       RS.DynPar.mode = 2;
-      RS.ekin(q0,qD0);
-      RS.epot(q0);
-      RS.gravload(q0);
-      RS.inertia(q0);
-      RS.corvec(q0,qD0);
-      RS.cormat(q0,qD0);
-      RS.invdyn(q0,qD0,qDD0);
+      T2 = RS.ekin(q0,qD0);
+      U2 = RS.epot(q0);
+      g2 = RS.gravload(q0);
+      M2 = RS.inertia(q0);
+      c2 = RS.corvec(q0,qD0);
+      C2 = RS.cormat(q0,qD0);
+      t2 = RS.invdyn(q0,qD0,qDD0);
+
       [w, wreg] = RS.internforce(q0,qD0,qDD0);
       f_test = reshape( wreg(1:3*RS.NL,:)*RS.DynPar.ipv_floatb, 3, RS.NL);
-      f_test - w(1:3,:);
+      test_f = f_test - w(1:3,:);
+      if any(abs(test_f(:)) > 1e-10)
+        error('Schnittkräfte stimmen nicht zwischen zwei Rechenwegen');
+      end
       m_test = reshape( wreg(3*RS.NL+1:end,:)*RS.DynPar.ipv_floatb, 3, RS.NL);
-      m_test - w(4:6,:);
+      test_m = m_test - w(4:6,:);
+      if any(abs(test_m(:)) > 1e-10)
+        error('Schnittmomente stimmen nicht zwischen zwei Rechenwegen');
+      end
+      
       RS.internforce_traj(repmat(q0',5,1),repmat(qD0',5,1),repmat(qDD0',5,1));
       RS.invdyn_traj(repmat(q0',5,1),repmat(qD0',5,1),repmat(qDD0',5,1));
       
+      % Rufe Dynamik-Funktionen mit Inertialparametern als Eingang auf und
+      % benutze die Regressorform
+      RS.DynPar.mode = 3;
+      [T3,Treg3]=RS.ekin(q0,qD0);
+      [U3,Ureg3]=RS.epot(q0);
+      [g3,greg3]=RS.gravload(q0);
+      [M3,Mreg3]=RS.inertia(q0);
+      [c3,creg3]=RS.corvec(q0,qD0);
+      [C3,Creg3]=RS.cormat(q0,qD0);
+      [t3,treg3]=RS.invdyn(q0,qD0,qDD0);
+      if abs(T3-Treg3*RS.DynPar.ipv) > 1e-10 || abs(T3-T2) > 1e-10
+        error('Regressor für kinetische Energie stimmt nicht mit DynPar Methode 3');
+      end
+      if abs(U3-Ureg3*RS.DynPar.ipv) > 1e-10 || abs(U3-U2) > 1e-10
+        error('Regressor für potentielle Energie stimmt nicht mit DynPar Methode 3');
+      end
+      if any(abs(g3-greg3*RS.DynPar.ipv) > 1e-10) || abs(U3-U2) > 1e-10
+        error('Regressor für Gravitationsmoment stimmt nicht mit DynPar Methode 3');
+      end
+      if any(abs(c3-creg3*RS.DynPar.ipv) > 1e-10) || any(abs(c3-c2) > 1e-10)
+        error('Regressor für Coriolismoment stimmt nicht mit DynPar Methode 3');
+      end
+      if any(abs(t3-treg3*RS.DynPar.ipv)> 1e-10)  || any(abs(t3-t2) > 1e-10)
+        error('Regressor für Inversdynamik-Moment stimmt nicht mit DynPar Methode 3');
+      end
+      
       % Rufe Dynamik-Funktionen mit Minimalparametervektor als Eingang auf
       RS.DynPar.mode = 4;
-      [T,Treg]=RS.ekin(q0,qD0);
-      [U,Ureg]=RS.epot(q0);
-      [g,greg]=RS.gravload(q0);
-      [M,Mreg]=RS.inertia(q0);
-      [c,creg]=RS.corvec(q0,qD0);
-      [C,Creg]=RS.cormat(q0,qD0);
-      [t,treg]=RS.invdyn(q0,qD0,qDD0);
+      [T4,Treg4]=RS.ekin(q0,qD0);
+      [U4,Ureg4]=RS.epot(q0);
+      [g4,greg4]=RS.gravload(q0);
+      [M4,Mreg4]=RS.inertia(q0);
+      [c4,creg4]=RS.corvec(q0,qD0);
+      [C4,Creg4]=RS.cormat(q0,qD0);
+      [t4,treg4]=RS.invdyn(q0,qD0,qDD0);
+      if abs(T4-Treg4*RS.DynPar.mpv) > 1e-10 || abs(T4-T2) > 1e-10
+        error('Regressor für kinetische Energie stimmt nicht mit DynPar Methode 4');
+      end
+      if abs(U4-Ureg4*RS.DynPar.mpv) > 1e-10 || abs(U4-U2) > 1e-10
+        error('Regressor für potentielle Energie stimmt nicht mit DynPar Methode 4');
+      end
+      if any(abs(g4-greg4*RS.DynPar.mpv) > 1e-10) || abs(U4-U2) > 1e-10
+        error('Regressor für Gravitationsmoment stimmt nicht mit DynPar Methode 4');
+      end
+      if any(abs(c4-creg4*RS.DynPar.mpv) > 1e-10) || any(abs(c4-c2) > 1e-10)
+        error('Regressor für Coriolismoment stimmt nicht mit DynPar Methode 4');
+      end
+      if any(abs(t4-treg4*RS.DynPar.mpv)> 1e-10)  || any(abs(t4-t2) > 1e-10)
+        error('Regressor für Inversdynamik-Moment stimmt nicht mit DynPar Methode 4');
+      end
+      
       RS.invdyn_traj(repmat(q0',5,1),repmat(qD0',5,1),repmat(qDD0',5,1));
       RS.invdyn2_traj(repmat(q0',5,1),repmat(qD0',5,1),repmat(qDD0',5,1));
       RV = RS.invdynregmat_traj(repmat(q0',5,1),repmat(qD0',5,1),repmat(qDD0',5,1));
