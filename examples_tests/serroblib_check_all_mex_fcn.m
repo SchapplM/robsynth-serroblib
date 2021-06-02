316727c49ca33abb096d8e2fb08b5c1ffe620519
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2021-06-02 10:00:21 +0100

    Funktionstest überarbeitet: Auch Prüfung der Vorlagen-Funktionen;

--- examples_tests/serroblib_check_all_mex_fcn.m
+++ examples_tests/serroblib_check_all_mex_fcn.m
@@ -2,7 +2,7 @@
 % der mex-Funktionen. Automatische Neu-Erzeugung, falls Syntax-Fehler.
 % Dieses Skript ist dann nützlich, wenn die Schnittstellen im Robotik-Repo
 % geändert wurden und noch mex-Dateien vorliegen, die mit der alten Version
-% kompiliert wurden.
+% kompiliert wurden. Es werden auch die zugrunde liegenden m-Dateien geprüft.
 
 % Moritz Schappler, moritz.schappler@imes.uni-hannover.de, 2021-04
 % (C) Institut für Mechatronische Systeme, Leibniz Universität Hannover
@@ -29,51 +29,72 @@ for N = 1:7 % Alle Gelenk-FG durchgehen
         tpl_dir = fullfile(repopath, sprintf('mdl_%ddof', N), Name_GenMdl, 'tpl');
       end
     end
+    % TODO: Hier muss mex/kein mex gleichermaßen geprüft werden. Eventuell
+    % muss neu generiert werden, wenn keine mex-Datei da ist. Wenn neu
+    % generiert wurde, müssen die nicht-mex-Funktionen nicht neu geprüft
+    % werden.
     mexfilelist = dir(fullfile(tpl_dir, '*_mex.mex*'));
-    if isempty(mexfilelist)
-      % Nichts zu prüfen. Es gibt keine mex-Dateien
+    mfilelist = dir(fullfile(tpl_dir, '*.m'));
+    filelist = [mexfilelist; mfilelist];
+    if isempty(filelist)
+      % Nichts zu prüfen. Es gibt keine Dateien
       continue
     end
-    fprintf('Prüfe Roboter %d/%d (%s) (%d mex-Dateien liegen in tpl-Ordner %s)\n', ...
-      j, length(l.Names_Ndof), Name, length(mexfilelist), tpl_dir)
+    fprintf('Prüfe Roboter %d/%d (%s) (%d mex-Dateien und %d m-Dateien liegen in tpl-Ordner %s)\n', ...
+      j, length(l.Names_Ndof), Name, length(mexfilelist), length(mfilelist), tpl_dir)
     RS = serroblib_create_robot_class(Name);
+    RS_mex_status = true;
     RS.fill_fcn_handles(true, false);
-    % Gehe alle mex-Dateien durch, die da sind.
-    for kk = 1:length(mexfilelist)
+    % Gehe alle mex- und m-Dateien durch, die da sind.
+    for kk = 1:length(filelist)
+      if ~contains(filelist(kk).name, '_mex')
+        % Prüfe ab jetzt die m-Dateien. Die mex-Dateien sind fertig.
+        if RS_mex_status == true
+          RS.fill_fcn_handles(false, false);
+          RS_mex_status = false;
+        end
+      end
       for retryiter = 1:3 % mehrfache Neuversuche zur Fehlerkorrektur
         recompile = false;
         % Einzelne Fälle für die mex-Dateien durchgehen und jeweils
         % Dummy-Aufruf der Funktionen, um Syntax-Fehler aufzudecken.
-        if contains(mexfilelist(kk).name, 'invkin_eulangresidual_mex')
+        if contains(filelist(kk).name, 'invkin_eulangresidual')
           try
-            RS.invkin2(NaN(3,4), zeros(RS.NJ,1));
+            RS.invkin2(eye(3,4), zeros(RS.NJ,1));
           catch err
-            recompile = true;
+            if ~strcmp(err.identifier, 'MATLAB:svd:matrixWithNaNInf')
+              recompile = true;
+            end
           end
         end
-        if contains(mexfilelist(kk).name, 'invkin_traj_mex')
+        if contains(filelist(kk).name, 'invkin_traj')
           try
-            RS.invkin2_traj(NaN(2,6), NaN(2,6), NaN(2,6), [0;1], zeros(RS.NQJ,1));
+            RS.invkin2_traj(zeros(2,6), zeros(2,6), zeros(2,6), [0;1], zeros(RS.NQJ,1));
           catch err
-            recompile = true;
+            if ~strcmp(err.identifier, 'MATLAB:svd:matrixWithNaNInf')
+              recompile = true;
+            end
           end
         end
         % Falls ein Fehler vorliegt, wird neu kompiliert oder generiert.
         if recompile
           warning('Fehler beim Aufruf von Funktion %s. Fehler: %s', ...
-            mexfilelist(kk).name, err.message);
+            filelist(kk).name, err.message);
           if retryiter == 2
             warning('Zweiter Versuch ohne Erfolg. Voraussichtlich sind die Vorlagen-Funktionen veraltet');
             serroblib_create_template_functions({Name}, false, false);
           elseif retryiter == 3
             error('Auch beim dritten Versuch kein Erfolg');
           end
-          [~,mexbasename] = fileparts(mexfilelist(kk).name);
-          matlabfcn2mex({mexbasename(1:end-4)}); % ohne "_mex"
+          if contains(filelist(kk).name, '_mex')
+            % Mex-Dateien werden neu kompiliert. m-Dateien nicht.
+            [~,mexbasename] = fileparts(filelist(kk).name);
+            matlabfcn2mex({mexbasename(1:end-4)}); % ohne "_mex"
+          end
         else
           % Alles funktioniert. Keine Neuversuche notwendig.
           if retryiter > 1
-            fprintf('Mex-Datei %s wurde korrigiert\n', mexfilelist(kk).name);
+            fprintf('Datei %s wurde korrigiert\n', filelist(kk).name);
           end
           break
         end % recompile
