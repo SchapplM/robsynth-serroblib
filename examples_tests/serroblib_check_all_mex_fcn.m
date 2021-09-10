3a2246dde6b5ec05189899917b23f9bab953ab12
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2021-09-10 12:03:06 +0200

    Anpassung des Testskripts an aktualisierte Funktionen

--- examples_tests/serroblib_check_all_mex_fcn.m
+++ examples_tests/serroblib_check_all_mex_fcn.m
@@ -62,8 +62,8 @@ for N = 1:7 % Alle Gelenk-FG durchgehen
             % Prüfe Dateiinhalte auf charakteristische Einträge
             if ~RS_mex_status % Nicht für mex-Dateien
               filetext = fileread(fullfile(tpl_dir, filelist(kk).name));
-              if ~contains(filetext, 'installspace_thresh')
-                error('Textfragment "installspace_thresh" nicht gefunden. Alte Version.');
+              if ~contains(filetext, 'Stats.maxcolldepth')
+                error('Textfragment "Stats.maxcolldepth" nicht gefunden. Alte Version.');
               end
             end
             % Führe die Funktion aus
@@ -86,8 +86,8 @@ for N = 1:7 % Alle Gelenk-FG durchgehen
             % Prüfe Dateiinhalte auf charakteristische Einträge
             if ~RS_mex_status % Nicht für mex-Dateien
               filetext = fileread(fullfile(tpl_dir, filelist(kk).name));
-              if ~contains(filetext, 'installspace_thresh')
-                error('Textfragment "installspace_thresh" nicht gefunden. Alte Version.');
+              if ~contains(filetext, 'Stats.h_coll_thresh')
+                error('Textfragment "Stats.h_coll_thresh" nicht gefunden. Alte Version.');
               end
             end
             % Führe die Funktion aus
